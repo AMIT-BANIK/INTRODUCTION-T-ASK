d087c55498dba09615e1464cc30d8daec3517e88
AMIT-BANIK <amitbanikspsc@gmail.com>
2024-12-10 01:53:26 +0600

    first commit

---
+++ src/AreaOfaTriangle.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.Scanner;
+public class AreaOfaTriangle {
+    public static void main(String[] args) {
+        float base, height, area;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the base");
+        base = input.nextFloat();
+        System.out.println("Enter the height");
+        height = input.nextFloat();
+        area = base * height * 0.5f;
+        System.out.println("The area of the triangle is : " + area);
+    }
+}
